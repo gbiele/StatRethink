39386bb77820cf74275c50f957835ddcf9c4a872
Guido Biele <guido.biele@neuro-cognition.org>
2022-02-14 15:14:11 +0100

    Add nearly complete doc

--- BayesProportions.R
+++ BayesProportions.R
@@ -1,29 +1,40 @@
-bayes_proportions = function(n.obs.prior = 2, p.success.prior = .5, n.obs.data = 10, p.success.data = .5) {
-  a.prior = n.obs.prior * p.success.prior
-  b.prior = n.obs.prior - a.prior
+bayes_proportions = function(n.prior = 2, p.prior = .5, n.data = 10, p.data = .5, max.y = NULL) {
+  a.prior = n.prior * p.prior
+  b.prior = n.prior - a.prior
   
-  a.data = n.obs.data * p.success.data
-  b.data = n.obs.data - a.data
+  a.data = n.data * p.data
+  b.data = n.data - a.data
   
   a.post = a.prior + a.data
   b.post = b.prior + b.data
   
-  x = seq(.001,.999,.001)
-  max.y = max(
-    dbeta(x,a.prior,b.prior),
-    dbeta(x,a.data,b.data),
-    dbeta(x,a.post,b.post)
-  )
+  if (is.null(max.y)) {
+    x = seq(.001,.999,.001)
+    max.y = max(
+      dbeta(x,a.prior,b.prior),
+      dbeta(x,a.data,b.data),
+      dbeta(x,a.post,b.post)
+    )
+  }
+  
+  params = 
+    paste0(
+      c("n.prior","p.prior","n.data", "p.data"),": ",
+      c(n.prior,p.prior,n.data, p.data))
+  params = paste0(paste(params[1:2],collapse = ", "),"\n",
+                 paste(params[3:4],collapse = ", "))
   
   curve(dbeta(x,a.prior,b.prior), col = "blue", 
-        ylim = c(0, max.y), bty = "none",
-        ylab = "density", xlab = "theta")
-  curve(dbeta(x,a.data,b.data), col = "red", add = T)
-  curve(dbeta(x,a.post,b.post), col = "purple", add = T)
+        ylim = c(0, max.y), bty = "none", lwd = 2,
+        ylab = "density", xlab = "theta", n = 500,
+        main = params)
+  curve(dbeta(x,a.data,b.data), col = "red", add = T, lwd = 2, n = 500)
+  curve(dbeta(x,a.post,b.post), col = "purple", add = T, lwd = 2, n = 500)
   
   legend("topright",
          lty = 1,
-         col = c("red","blue","purple"),
+         lwd = 2,
+         col = c("blue","red","purple"),
          legend = c("Prior","Likelihood","Posterior"),
-         bty = "none")
+         bty = "n")
 }
