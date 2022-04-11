f243b97527248440a180b56d20c39f72f43d3beb
gbiele <guido.biele@gmail.com>
2022-04-11 18:06:37 +0200

    minor updates and animation for residuals

---
+++ Chapter5/animation_residuals.R
@@ -0,0 +1,59 @@
+
+animate_residulas = function(m.B_D,d) {
+  x = d$D[idx]
+  y = d$C[idx]
+  
+  coefs = coef(m.B_D)
+  a = coefs[1]
+  resid = y - (coefs[1] + x*coefs[2])
+  
+  
+  bs = seq(coefs[2],0,length.out = 48)
+  bs = c(rep(coefs[2],25),bs)
+  k = 0
+  for (b in bs) {
+    k = k+1
+    p = a + x*b
+    y.tmp = p+resid
+    par(mar = c(3,3,0,1))
+    png(paste0("animation/anim",sprintf("%03d", k), ".png"),
+        width = 900, height = 750,pointsize = 24)
+    plot(0,type = "n", ylim = c(-3,3), xlim = range(x),
+         ylab = expression(B(estimated)~","~B[r]),
+         xlab = "D")
+    abline(a = a, b = b, col = "blue")
+    points(x,y.tmp)
+    arrows(x0 = x, y0 = p, y1 = p + resid,length = 0, col = "blue")
+    dev.off()
+  }
+  pp = 2
+  N2 = ceiling(length(y)/pp)
+  for (j in 1:N2) {
+    k = k+1
+    p = a + x*b
+    y.tmp = p+resid
+    par(mar = c(3,3,0,1))
+    png(paste0("animation/anim",sprintf("%03d", k), ".png"),
+        width = 900, height = 750,pointsize = 24)
+    plot(0,type = "n", ylim = c(-3,3), xlim = range(x),
+         ylab = expression(B(estimated)~","~B[r]),
+         xlab = "D")
+    abline(a = a, b = b, col = "blue")
+    points(x,y.tmp)
+    is = setdiff(1:length(y),1:(pp*j))
+    arrows(x0 = x[is], y0 = p[is], y1 = p[is] + resid[is],length = 0, col = "blue")
+    dev.off()
+  }
+  k = k+1
+  png(paste0("animation/anim",sprintf("%03d", k), ".png"),
+      width = 900, height = 750,pointsize = 24)
+  plot(0,type = "n", ylim = c(-3,3), xlim = range(x),
+       ylab = expression(B[r]),
+       xlab = "D")
+  points(x, a + resid)
+  dev.off()
+  
+  system(glue::glue("ffmpeg -y -pattern_type sequence -i animation/anim%03d.png -c:v libx264 -pix_fmt yuv420p residuals.mp4"))
+}
+
+  
