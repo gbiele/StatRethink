3d6ae8dfadec3f49930b5fbbe260008b23dd9883
Guido Biele <guido.biele@neuro-cognition.org>
2022-04-12 08:41:54 +0200

    update animation

--- Chapter5/animation_residuals.R
+++ Chapter5/animation_residuals.R
@@ -7,7 +7,7 @@ animate_residulas = function(m.B_D,d) {
   a = coefs[1]
   resid = y - (coefs[1] + x*coefs[2])
   
-  
+  black_blue_ramp = colorRampPalette(c("black", "blue"))
   bs = seq(coefs[2],0,length.out = 48)
   bs = c(rep(coefs[2],25),bs)
   k = 0
@@ -16,13 +16,18 @@ animate_residulas = function(m.B_D,d) {
     p = a + x*b
     y.tmp = p+resid
     par(mar = c(3,3,0,1))
+    clr = ifelse(k<26,"black",black_blue_ramp(48)[k-25])
     png(paste0("animation/anim",sprintf("%03d", k), ".png"),
         width = 900, height = 750,pointsize = 24)
     plot(0,type = "n", ylim = c(-3,3), xlim = range(x),
-         ylab = expression(B(estimated)~","~B[r]),
+         ylab = "",
          xlab = "D")
+    clr1 = adjustcolor("black", alpha = ifelse(k < 26, 1, 1-(k-25)/48 ))
+    clr2 = adjustcolor("black", alpha = ifelse(k < 26, 0, (k-25)/48 ))
+    mtext(expression(widehat(B)), side = 2, line = 2, col = clr1)
+    mtext(expression(B[r]), side = 2, line = 2, col = clr2)
     abline(a = a, b = b, col = "blue")
-    points(x,y.tmp)
+    points(x,y.tmp, col = clr)
     arrows(x0 = x, y0 = p, y1 = p + resid,length = 0, col = "blue")
     dev.off()
   }
@@ -36,10 +41,10 @@ animate_residulas = function(m.B_D,d) {
     png(paste0("animation/anim",sprintf("%03d", k), ".png"),
         width = 900, height = 750,pointsize = 24)
     plot(0,type = "n", ylim = c(-3,3), xlim = range(x),
-         ylab = expression(B(estimated)~","~B[r]),
+         ylab = expression(B[r]),
          xlab = "D")
     abline(a = a, b = b, col = "blue")
-    points(x,y.tmp)
+    points(x, y.tmp, col = "blue")
     is = setdiff(1:length(y),1:(pp*j))
     arrows(x0 = x[is], y0 = p[is], y1 = p[is] + resid[is],length = 0, col = "blue")
     dev.off()
@@ -50,7 +55,7 @@ animate_residulas = function(m.B_D,d) {
   plot(0,type = "n", ylim = c(-3,3), xlim = range(x),
        ylab = expression(B[r]),
        xlab = "D")
-  points(x, a + resid)
+  points(x, a + resid, col = "blue")
   dev.off()
   
   system(glue::glue("ffmpeg -y -pattern_type sequence -i animation/anim%03d.png -c:v libx264 -pix_fmt yuv420p residuals.mp4"))
